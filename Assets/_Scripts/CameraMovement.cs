3ef2d66e3aea3168fa975ca8ee307c7b2d1f7db1
GuglielmoGiuseppe <116627538+GuglielmoGiuseppe@users.noreply.github.com>
2025-03-17 15:58:37 +0100

    Cubi
    spawn Cobi

--- Assets/_Scripts/CameraMovement.cs
+++ Assets/_Scripts/CameraMovement.cs
@@ -7,8 +7,16 @@ public class CameraMovement : MonoBehaviour
     public int speed = 1;
     public float distance;
     Vector3 startPosition= Vector3.zero;
+
+    public GameObject cube;
+    public int initCubePosition = 299600;
+    public int initValue = 300;
+
+    public Transform parent;
+    
     private void Awake()
     {
+        InitCube();
         this.transform.position = startPosition;
     }
     private void FixedUpdate()
@@ -21,4 +29,24 @@ public class CameraMovement : MonoBehaviour
     {
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, changeDistance);
     }
+
+    public void InitCube() {
+
+        for (int i = 0; i < initValue; i += 2) {
+            Vector3 v = new Vector3(3, 0, 299600 + i);
+            GameObject newCube = Instantiate(cube, parent);
+            newCube.transform.position = v;
+            newCube.isStatic = true;
+        }
+        for (int i = 0; i < initValue; i += 2) {
+            Vector3 v = new Vector3(-3, 0, 299600 + i);
+            GameObject newCube = Instantiate(cube, parent);
+            newCube.transform.position = v;
+            newCube.isStatic = true;
+        }
+        
+        
+
+    }
+    
 }
